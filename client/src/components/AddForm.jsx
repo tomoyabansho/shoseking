1a6f39079774a64aa7f38c73de374d7718faa040
tomoyabansho <bansho@seesaa.co.jp>
2018-08-27 13:32:04 +0900

    be able to display 'From'

---
+++ client/src/components/AddForm.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { changeName, changeAge } from '../actions'
+
+const AddForm = ({ store }) => {
+  const { name, age } = store.getState().form  // storeからフォームの内容を取得
+
+  return (
+    <div>
+      <form>
+        <label>
+          名前:
+          <input value={name} onChange={e => store.dispatch(changeName(e.target.value))} />
+        </label>
+        <label>
+          年齢:
+          <input value={age} onChange={e => store.dispatch(changeAge(e.target.value))} />
+        </label>
+        <button type="submit">submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default AddForm
---
+++ client/src/components/CharacterList.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const CharacterList = ({ store }) => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default CharacterList
