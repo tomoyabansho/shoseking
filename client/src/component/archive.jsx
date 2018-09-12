72019ebd29d02543191c045f2454cc58ea486ccf
tomoyabansho <bansho@seesaa.co.jp>
2018-09-12 14:19:08 +0900

    use dummy image

--- client/src/component/archive.jsx
+++ client/src/component/archive.jsx
@@ -7,12 +7,13 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Archive = ({ id, date, title, writer, ...props}) => {
   return (<Media className="article">
-    <Media.Left>{ date }</Media.Left>
+    <Media.Left><img src='https://picsum.photos/300/450' /></Media.Left>
     <Media.Body>
       <Media.Heading>{ title }<small>{ writer }</small></Media.Heading>
       <p>
         { props.children }
       </p>
+      <p>{ date }</p>
       <Button onClick={()=>{
         EventBus.dispatch('delete', this, id)
       }}>Delete</Button>
