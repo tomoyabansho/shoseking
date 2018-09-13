f772c63be187daf9905b43c89953f62d43dffd1c
tomoyabansho <bansho@seesaa.co.jp>
2018-09-13 18:20:00 +0900

    use google books api and display dropdown list

--- client/src/component/search_book_form.jsx
+++ client/src/component/search_book_form.jsx
@@ -7,7 +7,26 @@ import 'semantic-ui-css/semantic.min.css';
 
 const SearchBooksForm = () => {
   this.state = []
+  const clear = () => {
+    this.state.splice(0, this.state.length)
+  }
+
+  EventBus.addEventListener('update dropdown', (event, items) => {
+    clear()
+    console.log(items)
+    items.map(item => {
+      const data = {}
+      data.key = item.id
+      data.value = item.id
+      data.flag = item.title
+      data.text = `${item.title} (${item.author})`
+      data.image = item.thumbnail
+      this.state.push(data)
+    })
+  })
   const onChangeAction = (event, data) => {
+    console.log(data)
+    console.log(event)
     EventBus.dispatch('select book', this, data.value)
   }
   const onSearchChangeAction = (event, data) => {
--- client/src/model/AppModel.js
+++ client/src/model/AppModel.js
@@ -4,6 +4,9 @@ import io from 'socket.io-client'
 
 class AppModel{
   constructor(){
+
+    this.items = []
+
     const socket = io('http://localhost:3001');
     socket.on('connect', () => {
       console.log('connected')
@@ -23,13 +26,41 @@ class AppModel{
       this.state = {
         date: null,
         title: "",
+        author: '',
+        image_url: '',
         writer: "",
         content: ""
       }
     })
 
     EventBus.addEventListener('change title', (event, query) => {
-      console.log(query)
+      this.now = new Date()
+      if(!this.execute || this.now - this.execute > 1000){
+        const url = `https://www.googleapis.com/books/v1/volumes?q=${ query }`
+        const array = []
+        axios.get(url)
+          .then(response => {
+            const items = response.data.items
+            items.map(({id, volumeInfo}) => {
+              const thumbnail = volumeInfo.imageLinks && volumeInfo.imageLinks.thumbnail
+                ? volumeInfo.imageLinks.thumbnail
+                : "https://picsum.photos/300/450"
+              const item = {
+                key: id,
+                title: volumeInfo.title,
+                author: volumeInfo.authors,
+                thumbnail: thumbnail
+              }
+              array.push(item)
+            })
+            this.items = array
+            EventBus.dispatch('update dropdown', this, this.items)
+          })
+          .catch(error => {
+            console.error(error)
+          })
+        this.execute = this.now
+        }
     })
 
     EventBus.addEventListener('select book', (event, value) => {
